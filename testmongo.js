9ec69e5ef070e9a9d59954cc826d4cb410010557
Alex Chaplain <111529711+AlexChaplain@users.noreply.github.com>
2024-02-29 20:45:59 -0600

    Update testmongo.js
    nonAdmin Added

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://alexchaplain:<password>@cluster1.4c770wa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
+const uri = "mongodb+srv://nonAdmin:<nonAdmin1>@cluster1.4c770wa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
@@ -14,7 +14,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // routes will go here
-
+// Route to access database:
+app.get('/api/mongo/:item', function(req, res) {
+}
 // Default route:
 app.get('/', function(req, res) {
   const myquery = req.query;
