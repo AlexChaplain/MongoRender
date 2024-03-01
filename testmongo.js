347e7d9c83cf31b4a84b7dc3dce0376904e71ee1
Alex Chaplain <111529711+AlexChaplain@users.noreply.github.com>
2024-02-29 21:00:42 -0600

    Update testmongo.js
    API-KEY

--- testmongo.js
+++ testmongo.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // routes will go here
 // Route to access database:
-app.get('/api/mongo/:item', function(req, res) {
+app.get('/qi3HHZ4dWBF8QQOrj58ddjqEcU4GkD1CqbnfumWX3hz1U9TsjJX6SqX3kLhKaF1D/mongo/:item', function(req, res) {
 }
 // Default route:
 app.get('/', function(req, res) {
